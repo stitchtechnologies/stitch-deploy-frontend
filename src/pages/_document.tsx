c526130f74d6c5b0d01dbc0c5421180d27aa94a8
zaini <alimzaini@outlook.com>
2024-02-15 15:15:20 -0800

    Add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon-light.svg" sizes="any" media="(prefers-color-scheme: light)" />
+        <link rel="icon" href="/favicon-dark.svg" sizes="any" media="(prefers-color-scheme: dark)" />
+      </Head>
       <body>
         <Main />
         <NextScript />
