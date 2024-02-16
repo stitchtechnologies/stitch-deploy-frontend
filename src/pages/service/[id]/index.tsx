2448673e20ae38e006c63cb2aca67bee6c35b15a
zaini <alimzaini@outlook.com>
2024-02-15 18:50:25 -0800

    Add icon

--- src/pages/service/[id]/index.tsx
+++ src/pages/service/[id]/index.tsx
@@ -1,8 +1,7 @@
 import Layout from "@/components/layout";
-import { ORGANIZATIONS } from "@/components/organization";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { ChevronRight, GitBranch, MoreHorizontal } from "lucide-react";
+import { GitBranch, MoreHorizontal } from "lucide-react";
 import {
     DropdownMenu,
     DropdownMenuCheckboxItem,
@@ -31,13 +30,13 @@ export default function Services() {
             </Head>
             <div className="flex items-center border-b-[rgba(0,0,0,0.10)] px-24 border-b border-solid">
                 <div className="flex items-center gap-4 my-12">
-                    <Link href={`/organization/${id}`}>
+                    <Link href={`/service/${id}`}>
                         <Avatar className="h-8 w-8">
                             <AvatarImage src={SERVICE.imageUrl} />
                             <AvatarFallback>{SERVICE.fallbackName}</AvatarFallback>
                         </Avatar>
                     </Link>
-                    <Link href={`/organization/${id}`}>
+                    <Link href={`/service/${id}`}>
                         <div className="text-4xl">
                             {SERVICE.serviceName}
                         </div>
@@ -74,7 +73,10 @@ export default function Services() {
                 <div className="grid grid-cols-3 space-y-2 bg-white rounded shadow p-6">
                     <div className="flex flex-col gap-1">
                         <h2 className="text-sm text-slate-500">Source</h2>
-                        <div className="text-sm mb-3 font-robotomono"><span className="text-slate-400">supabase/</span>supabase</div>
+                        <div className="text-sm mb-3 font-robotomono flex gap-2 items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" /></svg>
+                            <span><span className="text-slate-400">supabase/</span>supabase</span>
+                        </div>
                     </div>
                     <div className="flex flex-col gap-1">
                         <h2 className="text-sm text-slate-500">Branch</h2>
