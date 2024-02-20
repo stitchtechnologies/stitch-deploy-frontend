92e91f17f1633f3ace8a41bfcd67786eb7f992af
zaini <alimzaini@outlook.com>
2024-02-20 11:41:01 -0800

    Fix dark favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon-light.svg" sizes="any" media="(prefers-color-scheme: darl)" />
+        <link rel="icon" href="/favicon-light.svg" sizes="any" media="(prefers-color-scheme: dark)" />
         <link rel="icon" href="/favicon-dark.svg" sizes="any" media="(prefers-color-scheme: light)" />
       </Head>
       <body>
