6e09f0b690eb53735e50dfe12648722a9378cec2
zaini <alimzaini@outlook.com>
2024-02-22 11:49:11 -0800

    Add PostHog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,17 @@ import "@/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { AppProps } from "next/app";
 import { Inter, Roboto_Mono } from "next/font/google"
+import posthog from "posthog-js"
+import { PostHogProvider } from 'posthog-js/react'
+
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.debug() // debug mode in development
+    },
+  })
+}
 
 const inter = Inter({
   subsets: ['latin'],
@@ -17,7 +28,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`${inter.variable} ${robotoMono.variable} font-inter`}>
       <ClerkProvider {...pageProps}>
-        <Component {...pageProps} />
+        <PostHogProvider client={posthog}>
+          <Component {...pageProps} />
+        </PostHogProvider>
       </ClerkProvider>
     </main>
   )
